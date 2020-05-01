a22cee08cedebb31ef54131dc648d93606a1292a
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-05-01 15:24:37 +0300

    Initial commit

---
+++ Lox/Lox.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Lox
+{
+    internal class Lox
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: Lox [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string path)
+        {
+            Run(File.ReadAllText(path));
+        }
+
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                Run(Console.ReadLine());
+            }
+        }
+
+        private static void Run(string source)
+        {
+            // to be continued...
+        }
+    }
+}
